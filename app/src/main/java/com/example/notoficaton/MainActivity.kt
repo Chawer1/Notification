6dbd09c2281ab1d55a920b0d3e77d16d8414b7d1
Sergey Savitsky <sergeysavitsky22@gmail.com>
2019-11-21 06:40:29 +0300

    Merge branch 'master' of https://github.com/Chawer1/Notificaton
    Signed-off-by: Sergey Savitsky <sergeysavitsky22@gmail.com>

--- app/src/main/java/com/example/notoficaton/MainActivity.kt
+++ app/src/main/java/com/example/notoficaton/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.example.myapplication
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
@@ -8,21 +12,41 @@ import androidx.core.app.NotificationManagerCompat
 import com.example.notoficaton.R
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val button = findViewById<Button>(R.id.show)
+        createNotificationChannel()
         button.setOnClickListener {
             val builder =
                 NotificationCompat.Builder(this@MainActivity, CHANNEL_ID)
                     .setSmallIcon(R.drawable.ic_launcher_foreground)
-                    .setContentTitle("Напоминание")
-                    .setContentText("Пора покормить кота")
+                    .setContentTitle("Motivation title")
+                    .setContentText("Some motivation text")
                     .setPriority(NotificationCompat.PRIORITY_DEFAULT)
             val notificationManager =
                 NotificationManagerCompat.from(this@MainActivity)
             notificationManager.notify(NOTIFY_ID, builder.build())
         }
+
+
+    }
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "aSFASFASFASF"
+            val descriptionText = "OUTQAWTASTST"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+
     }
 
     companion object {
