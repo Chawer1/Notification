76f01bf5ad07737db59e866039f502366fad565d
Sergey Savitsky <sergeysavitsky22@gmail.com>
2019-11-21 04:24:18 +0300

    Merge branch 'master' of https://github.com/Chawer1/Notificaton
    Signed-off-by: Sergey Savitsky <sergeysavitsky22@gmail.com>

--- app/src/main/java/com/example/notoficaton/MainActivity.kt
+++ app/src/main/java/com/example/notoficaton/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.example.notoficaton
 
+import android.app.Notification
+import android.app.NotificationManager
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.NotificationCompat
 
 class MainActivity : AppCompatActivity() {
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
+    val notification: Notification = NotificationCompat.Builder(this)
+        .setSmallIcon(R.drawable.ic_main_notification)
+        .setContentTitle("Motivation notification")
+        .setContentText("Some motivation text here")
+        .build()
+
+
+    val notificationManager: Any? = getSystemService(NOTIFICATION_SERVICE)
 }
