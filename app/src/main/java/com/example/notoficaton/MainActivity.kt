713d208f6167775fdb00466f35adc2c64a093986
Sergey Savitsky <sergeysavitsky22@gmail.com>
2019-11-21 06:30:53 +0300

    Merge branch 'master' of https://github.com/Chawer1/Notificaton
    Signed-off-by: Sergey Savitsky <sergeysavitsky22@gmail.com>

--- app/src/main/java/com/example/notoficaton/MainActivity.kt
+++ app/src/main/java/com/example/notoficaton/MainActivity.kt
@@ -1,24 +1,32 @@
-package com.example.notoficaton
+package com.example.myapplication
 
-import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
-import android.content.Context
-import android.content.Intent
-import android.os.Build
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
-
+import com.example.notoficaton.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.show)
+        button.setOnClickListener {
+            val builder =
+                NotificationCompat.Builder(this@MainActivity, CHANNEL_ID)
+                    .setSmallIcon(R.drawable.ic_launcher_foreground)
+                    .setContentTitle("Напоминание")
+                    .setContentText("Пора покормить кота")
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            val notificationManager =
+                NotificationManagerCompat.from(this@MainActivity)
+            notificationManager.notify(NOTIFY_ID, builder.build())
+        }
+    }
 
+    companion object {
+        private const val NOTIFY_ID = 101
+        private const val CHANNEL_ID = "com.example.myapplication;"
     }
 }
